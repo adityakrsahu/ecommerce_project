dd5e0d0cd2cba1dbc65a80c67b7b59458833aacb
aditya kumar <adityakr2019@gmail.com>
2024-07-18 23:17:21 +0530

    models change

--- shopping/app/models.py
+++ shopping/app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class State(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    door_flat_no = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    city = models.CharField(max_length=100)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+    zip_code = models.CharField(max_length=10)
+    image = models.ImageField(upload_to='profile_images/')
+
+    def __str__(self):
+        return self.name
--- shopping/shopping/settings.py
+++ shopping/shopping/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
