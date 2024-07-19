2dc83d9389cfe22236c517bebd70e628f178f22c
aditya kumar <adityakr2019@gmail.com>
2024-07-20 00:35:00 +0530

    model cart

--- shopping/app/models.py
+++ shopping/app/models.py
@@ -41,9 +41,17 @@ class Product(models.Model):
     description=models.TextField()
     brand=models.CharField(max_length=100)
     category = models.CharField(choices=CATEGORY_CHOICES,max_length=2)
-    Product_image=models.ImageField(upload_to='productimg')
+    product_image=models.ImageField(upload_to='productimg')
 
     def __str__(self) -> str:
         return str(self.id)
     
+class Cart(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    product =  models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self) -> str:
+        return str(self.id)
+
 
