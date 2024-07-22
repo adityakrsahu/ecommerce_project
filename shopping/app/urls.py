dec62bbee2fa1300a5613e4ade1d286cd72f32ff
aditya kumar <adityakr2019@gmail.com>
2024-07-23 00:08:53 +0530

    password change

--- shopping/app/urls.py
+++ shopping/app/urls.py
@@ -5,7 +5,7 @@ from .forms import *
 
 urlpatterns = [
     path("", ProductView.as_view(), name='home'),
-    path('product-detail/<int:pk>', ProductDetailView.as_view(), name='product-detail'),
+    path('product-detail/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
     path('cart/', add_to_cart, name='add-to-cart'),
     path('buy/', buy_now, name='buy-now'),
     path('mobile/', mobile, name='mobile'),
@@ -19,8 +19,18 @@ urlpatterns = [
     path('profile/', profile, name='profile'),
     path('address/', address, name='address'),
     path('orders/', orders, name='orders'),
-    path('changepassword/', auth_views.PasswordChangeView.as_view(template_name='app/PasswordChange.html', form_class=PasswordChangeForm), name='changepassword'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
+    path('changepassword/', auth_views.PasswordChangeView.as_view(
+        template_name='app/PasswordChange.html', 
+        form_class=PasswordChangeForm, 
+        success_url='/passwordchangedone/'
+    ), name='changepassword'),
+    path("passwordchangedone/", auth_views.PasswordChangeDoneView.as_view(
+        template_name='app/passwordchangedone.html'
+    ), name="passwordchangedone"),
+    path('accounts/login/', auth_views.LoginView.as_view(
+        template_name='app/login.html', 
+        authentication_form=LoginForm
+    ), name='login'),
     path('registration/', CustomerRegistrationView.as_view(), name='customerregistration'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
